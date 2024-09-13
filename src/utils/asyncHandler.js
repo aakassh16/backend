fcc88cfa1721497856e6d93299bfbb6d74b5f59a
Akash Kundu <aakassh.uit@gmail.com>
2024-09-13 13:02:06 +0530

    updated

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+
+export const asyncHandler = (requestHandler) => {
+    (req, res, next) =>{
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+
+// export const asyncHandler = (fn) => async (req, res, next) => {
+//     try{
+//         await fn(req, re, next)
+//     }catch (error) {
+//         res.status( err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
+
+
